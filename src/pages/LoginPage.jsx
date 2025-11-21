c135f3fac110e137b9e90d673a66a602c10efa14
htetzawphyo <htetzawphyo.hpn@gmail.com>
2025-11-21 11:14:37 +0630

    add name

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -109,7 +109,7 @@ const LoginPage = () => {
             color: "#d81b60", // Deep pink text
           }}
         >
-          Login အသစ်ချိန်း
+          Login
         </Typography>
 
         <Box
