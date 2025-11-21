adc099ee2ba38b63c9f5dd991057c361ffc3c523
htetzawphyo <htetzawphyo.hpn@gmail.com>
2025-11-21 11:06:36 +0630

    lori

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -109,7 +109,7 @@ const LoginPage = () => {
             color: "#d81b60", // Deep pink text
           }}
         >
-          Login
+          Login Lori Gaming
         </Typography>
 
         <Box
